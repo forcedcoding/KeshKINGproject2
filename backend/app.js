4da5828ff7c467fc375e4d7e3ce5df0dc6cc1144
forcedcoding <shashwatar.9876@gmail.com>
2025-04-16 11:50:28 +0530

    ok

--- backend/app.js
+++ backend/app.js
@@ -16,3 +16,24 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 app.listen(9000,()=>{
     console.log('Server started on port 9000');
 });
+
+//design book schema
+const BookSchema = new mongoose.Schema({
+    title : String,
+    author : String,
+    date : String,
+    image : String
+});
+
+// design book model
+const Book = mongoose.model('Book', BookSchema);
+
+app.post('/books', async (req, res) => {
+    const newBook = new Book(req.body);
+    try {
+        const savedBook = await newBook.save();
+        res.status(200).json(savedBook).send('Book added successfully');
+    } catch (err) {
+        res.status(500).json(err).send('Error adding book');
+    }
+});
